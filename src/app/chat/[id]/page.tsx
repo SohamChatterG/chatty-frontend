423ceb201f3a2240e90aa3a10c54439758181e92
Soham Chatterjee <sohamchattopadhyay54@gmail.com>
2025-08-17 02:31:42 +0530

    .

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,20 +1,18 @@
+
 import ChatBase from '@/app/components/chat/ChatBase'
 import { fetchChatGroup, fetchChatGroupUsers } from '@/fetch/groupFetch'
 import { notFound } from 'next/navigation'
 import React from 'react'
 import { fetchChats } from '@/fetch/chatsFetch'
 import { ChatGroupType, GroupChatUserType, MessageType } from '@/types'
+
 type Props = {
     params: {
         id: string;
     };
 };
 
-async function Chat({
-    params,
-}: {
-    params: { id: string };
-}) {
+export default async function Chat({ params }: Props) {
 
     if (params.id.length !== 36) {
         return notFound()
@@ -23,14 +21,13 @@ async function Chat({
     if (group === null) {
         return notFound()
     }
-    const chats: Array<MessageType> | [] = await fetchChats(params.id)
+    const chats: Array<MessageType> | [] = await fetchChats(params.id);
 
     const user: Array<GroupChatUserType> | [] = await fetchChatGroupUsers(params.id);
+
     return (
         <div>
             <ChatBase fetchedUsers={user} group={group} oldMessages={chats} />
         </div>
     )
 }
-
-export default Chat
